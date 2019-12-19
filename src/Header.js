8a059230cb5f95745aa507c4e9fbd945fd7b9c8c
Charlotte House <cdmhouse@sky.com>
2019-12-19 20:53:21 +0000

    Playing with React

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+        <div>
+            <h1>This would be my Header</h1>
+            <h2> This would be my sub</h2>
+        </div>
+    );
+  }
+}
+export default Header;
