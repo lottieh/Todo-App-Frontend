d89298224d713d7ee75deb3969ae615a3993542b
Charlotte House <cdmhouse@sky.com>
2020-01-13 22:26:55 +0000

    Add button click added

--- src/1stlvl/InsertionSectionSubmit.js
+++ src/1stlvl/InsertionSectionSubmit.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 class InsertionSectionSubmit extends React.Component {
     
+
+    addClicked =()=> {
+        alert ('Oooh we are adding something !!!')
+    }
+
     render() {
         return (
             <div>
         <div className="col-12 col-md-1">
-                    <input type="submit" value="Add"/>
+                    <input type="submit" value="Add" onClick={this.addClicked}/>
                 </div>
             </div>
         )}}
