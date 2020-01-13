7cb722e8d53137cfc39cea7c0028d5f7cf7081e4
Charlotte House <cdmhouse@sky.com>
2020-01-13 22:33:49 +0000

    Clear background penguin added

--- src/1stlvl/NailingItPenguin.js
+++ src/1stlvl/NailingItPenguin.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import nailingitpenguin from '../nailingitpenguin.jpg';
+import Penguin2 from '../Penguin2.png';
 class NailingItPenguin extends React.Component {
 
     render() {
         return (
             <div className="row d-none d-xl-block">
                 <div className="col-12 col-md-6 penguin_image">
-                    <img src={nailingitpenguin} width="250"
+                    <img src={Penguin2} width="250"
                         alt="You're Nailing it penguin">
 
                     </img>
