13f4013f5fc7543beafcc394664b3aa679ce5932
Charlotte House <cdmhouse@sky.com>
2020-01-26 15:54:54 +0000

    Date picker changed back

--- src/1stlvl/InsertionDateform.js
+++ src/1stlvl/InsertionDateform.js
@@ -2,18 +2,22 @@ import React from 'react';
 import DatePicker from 'react-date-picker';
 
 class InsertionDateform extends React.Component {
-    
+
     render() {
         return (
-           
+            <div>
                 <div className="insert-col-4 Col sm={6}">
-                   <div className= 'input'>
-                    <DatePicker />
-                    </div> 
-                    <small id="dateHelp" className="form-text text-muted">
-                        Please enter date of completion if necessary.
-                    </small>
+
+                    <form action="/action_page.php">
+                        <input type="date" name="completionDate" />
+                    </form>
                 </div>
-                )
-                }}
-    export default InsertionDateform
+
+                <small id="dateHelp" className="form-text text-muted">
+                    Please enter date of completion if necessary.
+                    </small>
+            </div>
+        );
+    }
+}
+export default InsertionDateform
