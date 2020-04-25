b89e8379836008acd5c2e8fb123a068a59b81e26
Charlotte House <cdmhouse@sky.com>
2020-04-25 13:49:50 +0100

    undone Task fixed- desc wasn't being passed through

--- src/1stlvl/TaskDone.js
+++ src/1stlvl/TaskDone.js
@@ -4,7 +4,7 @@ import Emoji from '../Emoji';
 class TaskDone extends React.Component {
 
     undoneClicked = () => {
-        this.props.undoneTaskFunc(this.props.item.taskId);
+        this.props.undoneTaskFunc(this.props.item.taskId,this.props.item.description);
 }
     render() {
         return (
